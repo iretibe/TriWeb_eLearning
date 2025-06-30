ace1a79b5b476834fe922ff5501a0e78a288a88d
Somad Yessoufou <yessouf2009@yahoo.fr>
2025-06-30 13:51:08 +0000

    Deleted a folder

--- src/eLearning.Web/eLearning.Web.Client/Models/CourseModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace eLearning.Web.Client.Models
-{
-    public class CourseModel : CreateCourseModel
-    {
-        public bool IsRetired { get; set; }
-        public DateTime? RetireDate { get; set; }
-        public string LecturerId { get; set; } = string.Empty;
-    }
-}
